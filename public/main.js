2933a1003a4096033a8e781afdfeac206143c6c9
Leo Habb <LoaiHabb@gmail.com>
2024-06-25 23:57:34 +0300

    Update Appointments

--- public/main.js
+++ public/main.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   monthSelect.value = selectedMonth + 1;
 
   
-  updateAppointmentList(selectedMonth, currentYear)
+  updateAppointmentList(currentMonth, currentYear)
   updateTotal(selectedMonth, currentYear);
   updateCalendar(selectedMonth, currentYear);
 
